4d5cea2bc457bd35d6133a604ab33e0f712bffe3
likelian <likelian@me.com>
2022-04-16 17:47:15 -0400

    finish up a simple CNN

--- src/train/fader_net.py
+++ src/train/fader_net.py
@@ -2,65 +2,52 @@ import os
 import torch
 import torch.nn as nn
 import numpy as np
+from tqdm import tqdm
 
 
-device = set_device()
+device = torch.device('cpu')
 
-# Create folder with training, testing and validation data.
-
-spectrograms_dir = "Data/images_original/"
-folder_names = ['Data/train/', 'Data/test/', 'Data/val/']
-train_dir = folder_names[0]
-test_dir = folder_names[1]
-val_dir = folder_names[2]
-
-for f in folder_names:
-  if os.path.exists(f):
-    shutil.rmtree(f)
-    os.mkdir(f)
-  else:
-    os.mkdir(f)
-
-
-
-
-train_dataset = datasets.ImageFolder(
-    train_dir,
-    transforms.Compose([
-        transforms.ToTensor(),
-    ]))
-
+data_path = "../../../musdb18hq/"
+train_dataset = torch.load(data_path+'/train.pt')
 train_loader = torch.utils.data.DataLoader(
     train_dataset, batch_size=25, shuffle=True, num_workers=0)
 
-val_dataset = datasets.ImageFolder(
-    val_dir,
-    transforms.Compose([
-        transforms.ToTensor(),
-    ]))
+test_dataset = torch.load(data_path+'/test.pt')
+test_loader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=25, shuffle=True, num_workers=0)
 
-val_loader = torch.utils.data.DataLoader(
-    val_dataset, batch_size=25, shuffle=True, num_workers=0)
 
 
 # Make a CNN & train it to predict genres.
 
-class Fader_Net(nn.Module):
+class FaderNet(nn.Module):
   def __init__(self):
     """Intitalize neural net layers"""
-    super(music_net, self).__init__()
-    self.conv1 = nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, stride=1, padding=0)
+    super(FaderNet, self).__init__()
+    self.conv1 = nn.Conv2d(in_channels=2, out_channels=8, kernel_size=3, stride=1, padding=0)
     self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=0)
     self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=0)
     self.conv4 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=0)
     self.conv5 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=0)
-    self.fc1 = nn.Linear(in_features=9856, out_features=10)
+    self.fc1 = nn.Linear(in_features=768, out_features=1)
 
     self.batchnorm1 = nn.BatchNorm2d(num_features=8)
     self.batchnorm2 = nn.BatchNorm2d(num_features=16)
     self.batchnorm3 = nn.BatchNorm2d(num_features=32)
     self.batchnorm4 = nn.BatchNorm2d(num_features=64)
     self.batchnorm5 = nn.BatchNorm2d(num_features=128)
+
+    self.relu1 = nn.ReLU()
+    self.relu2 = nn.ReLU()
+    self.relu3 = nn.ReLU()
+    self.relu4 = nn.ReLU()
+    self.relu5 = nn.ReLU()
+
+    self.max_pool2d1 = nn.MaxPool2d(kernel_size=2)
+    self.max_pool2d2 = nn.MaxPool2d(kernel_size=2)
+    self.max_pool2d3 = nn.MaxPool2d(kernel_size=2)
+    self.max_pool2d4 = nn.MaxPool2d(kernel_size=2)
+    self.max_pool2d5 = nn.MaxPool2d(kernel_size=2)
 
     self.dropout = nn.Dropout(p=0.3, inplace=False)
 
@@ -69,103 +56,135 @@ class Fader_Net(nn.Module):
     # Conv layer 1.
     x = self.conv1(x)
     x = self.batchnorm1(x)
-    x = F.relu(x)
-    x = F.max_pool2d(x, kernel_size=2)
+    x = self.relu1(x)
+    x = self.max_pool2d1(x)
 
     # Conv layer 2.
     x = self.conv2(x)
     x = self.batchnorm2(x)
-    x = F.relu(x)
-    x = F.max_pool2d(x, kernel_size=2)
+    x = self.relu2(x)
+    x = self.max_pool2d2(x)
 
     # Conv layer 3.
     x = self.conv3(x)
     x = self.batchnorm3(x)
-    x = F.relu(x)
-    x = F.max_pool2d(x, kernel_size=2)
+    x = self.relu3(x)
+    x = self.max_pool2d3(x)
 
     # Conv layer 4.
     x = self.conv4(x)
     x = self.batchnorm4(x)
-    x = F.relu(x)
-    x = F.max_pool2d(x, kernel_size=2)
+    x = self.relu4(x)
+    x = self.max_pool2d4(x)
 
     # Conv layer 5.
-    x = self.conv5(x)
-    x = self.batchnorm5(x)
-    x = F.relu(x)
-    x = F.max_pool2d(x, kernel_size=2)
+    #x = self.conv5(x)
+    #x = self.batchnorm5(x)
+    #x = self.relu5(x)
+    #x = self.max_pool2d5(x)
 
     # Fully connected layer 1.
     x = torch.flatten(x, 1)
     x = self.dropout(x)
     x = self.fc1(x)
-    x = F.softmax(x)
+    x = torch.squeeze(x)
 
     return x
 
 
-def train(model, device, train_loader, validation_loader, epochs):
-  #criterion =  nn.CrossEntropyLoss()
+def train(model, device, train_loader, test_loader, epochs):
+
   loss = nn.MSELoss()
+  t_loss = nn.MSELoss()
+
   optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
-  train_loss, validation_loss = [], []
-  train_acc, validation_acc = [], []
+
+  train_loss, test_loss = [], []
+
   with tqdm(range(epochs), unit='epoch') as tepochs:
     tepochs.set_description('Training')
     for epoch in tepochs:
       model.train()
       # keep track of the running loss
       running_loss = 0.
-      correct, total = 0, 0
 
       for data_acc, data_vox, target in train_loader:
-        # getting the training set
-        data, target = data.to(device), target.to(device)
+          # getting the training set
+          data_acc, data_vox, target = data_acc.to(device), data_vox.to(device), target.to(device)
 
-        pred = model(data)
-        optimizer.zero_grad()
-        output = loss(pred, target)
-        output.backward()
-        optimizer.step()
+          data_acc = torch.nn.functional.normalize(data_acc)
+          data_vox = torch.nn.functional.normalize(data_vox)
 
-        tepochs.set_postfix(loss=loss.item())
-        running_loss += loss  # add the loss for this batch
+          data_acc *= torch.rand(1)
+          data_vox *= torch.rand(1)
 
-        # get accuracy
-        _, predicted = torch.max(output, 1)
-        total += target.size(0)
-        correct += (predicted == target).sum().item()
+          data = torch.stack((data_acc, data_vox), dim=0)
+          data = data.permute(1, 0, 2, 3) #batch, channel, time_step, mel_bank
+
+          pred = model(data)
+          optimizer.zero_grad()
+          MSE = loss(pred, target)
+          MSE.backward()
+          optimizer.step()
+
+          #tepochs.set_postfix(loss=loss.item())
+          running_loss += MSE.item()  # add the loss for this batch
+
 
       # append the loss for this epoch
       train_loss.append(running_loss/len(train_loader))
-      train_acc.append(correct/total)
 
-      # evaluate on validation data
+      print("epoch", epoch)
+      print("train_loss", running_loss/len(train_loader))
+
+
+      # evaluate on test data
       model.eval()
       running_loss = 0.
-      correct, total = 0, 0
 
-      for data, target in validation_loader:
-        # getting the validation set
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        tepochs.set_postfix(loss=loss.item())
-        running_loss += loss.item()
-        # get accuracy
-        _, predicted = torch.max(running_loss, 1)
-        total += target.size(0)
-        correct += (predicted == target).sum().item()
+      for test_acc, test_vox, test_target in test_loader:
+          # getting the validation set
+          test_acc, test_vox, test_target = test_acc.to(device), test_vox.to(device), test_target.to(device)
 
-      validation_loss.append(running_loss/len(validation_loader))
-      validation_acc.append(correct/total)
+          test_acc = torch.nn.functional.normalize(test_acc)
+          test_vox = torch.nn.functional.normalize(test_vox)
 
-  return train_loss, train_acc, validation_loss, validation_acc
+          test_data = torch.stack((test_acc, test_vox), dim=0)
+          test_data = test_data.permute(1, 0, 2, 3) #batch, channel, time_step, mel_bank
+
+          optimizer.zero_grad()
+          test_pred = model(test_data)
+
+          test_MSE = t_loss(test_pred, test_target)
+          #tepochs.set_postfix(loss=loss.item())
+          running_loss += test_MSE.item()
+
+      test_loss.append(running_loss/len(test_loader))
+
+      print("test_loss", running_loss/len(test_loader))
+
+  return train_loss, validation_loss
+
+
+net = FaderNet().to(device)
+train_loss, test_loss = train(net, device, train_loader, test_loader, 50)
 
 
 
-net = Fader_Net().to(device)
-train_loss, train_acc, validation_loss, validation_acc = train(net, device, train_loader, val_loader, 50)
-plot_loss_accuracy(train_loss, train_acc, validation_loss, validation_acc)
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+g = sns.relplot(x="epochs", y="MSE Loss", kind="line", data=train_loss)
+g.fig.suptitle('Train Loss')
+plt.show()
+plt.savefig('../../results/Train Loss.png')
+
+
+g = sns.relplot(x="epochs", y="MSE Loss", kind="line", data=test_loss)
+g.fig.suptitle('Test Loss')
+plt.show()
+plt.savefig('../../results/Test Loss.png')
