9e761782605cc160257ee7619cf07f8d12b3e504
“likelian” <likelian@me.com>
2022-09-12 00:08:45 -0400

    loudness distribution

---
+++ script/loudnessDistribution.py
@@ -0,0 +1,34 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+device = torch.device('cuda')
+
+data_path = "../../musdb18hq/"
+train_dataset = torch.load(data_path+'/train.pt')
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=25, shuffle=False, num_workers=0)
+
+test_dataset = torch.load(data_path+'/test.pt')
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=25, shuffle=False, num_workers=0)
+
+relative_loudness_list = []
+previous = 0
+for data_acc, data_vox, target in test_loader:
+    target_relative_loudness = target[0].cpu().detach().numpy()
+    if target_relative_loudness != previous:
+        relative_loudness_list.append(target_relative_loudness)
+
+relative_loudness_array = np.asarray(relative_loudness_list)
+averge_relative_loudness = np.mean(relative_loudness_array)
+
+print(averge_relative_loudness)
+
+plt.hist(relative_loudness_array, density=True, bins=100)
+
+plt.savefig('../results/loudnessDistribution.png')
+plt.close()
+
+
+
