1d9f73b78124701b3708cdf6be17416e1cbaad09
“likelian” <likelian@me.com>
2022-09-17 14:04:33 -0400

    mel spec of GTZAN, integrated loudness

--- src/data_collection/mel_spec.py
+++ src/data_collection/mel_spec.py
@@ -3,25 +3,38 @@ from os import walk
 import json
 import torch
 import torchaudio
+import soundfile as sf
+import numpy as np
+import sys
+import pyloudnorm as pyln
 
-def mel_spec(audio_path, output_path, ground_truth_dict):
+def mel_spec(audio_path, output_path):
     """
     create mel_spec tensor and level ratio ground truth
     """
 
-    abs_audio_path = os.path.abspath(audio_path)
+    #abs_audio_path = os.path.abspath(audio_path)
+    abs_audio_path = audio_path
 
     #win_length = n_fft
     #hop_length = win_length // 2
     transform = torchaudio.transforms.MelSpectrogram(sample_rate=44100, n_fft=2048)
 
+    
+
 
     for (dirpath, dirnames, filenames) in walk(abs_audio_path):
 
-        if "mixture.wav" in filenames \
+        #musdb19hq dataset
+        #if "mixture.wav" in filenames \
+        #    and "vocals.wav" in filenames:
+
+        #GTZAN dataset
+        if "no_vocals.wav" in filenames \
             and "vocals.wav" in filenames:
 
-            mxiture_path = dirpath+"/mixture.wav"
+            #mixture_path = dirpath+"/mixture.wav"
+            acc_path = dirpath+"/no_vocals.wav"
             vox_path = dirpath+"/vocals.wav"
 
             path_str = str(dirpath)
@@ -29,27 +42,60 @@ def mel_spec(audio_path, output_path, ground_truth_dict):
             filename = path_str[index+1:]
             print(filename)
 
-            ground_truth = ground_truth_dict[filename+"--vox_acc_ratio"]
-
 
         else:
             continue
 
-        mxiture, sample_rate = torchaudio.load(mxiture_path)
+        #mxiture, sample_rate = torchaudio.load(mxiture_path)
+        acc, sample_rate = torchaudio.load(acc_path)
         vox, sample_rate = torchaudio.load(vox_path)
 
-        acc = mxiture - vox
+        #acc = mxiture - vox
 
+        #mono
         acc = torch.mean(acc, 0)
         vox = torch.mean(vox, 0)
 
         acc_mel_spec = transform(acc)
         vox_mel_spec = transform(vox)
 
+        meter = pyln.Meter(44100)
+
+
+        acc = acc.detach().numpy()
+        vox = vox.detach().numpy()
+
+        #extract snippet relative loudness
+        #vox_acc_ratio_list = []
+        #for i in range(int(acc.shape[0] / 65536)):
+        #    acc_loudness = meter.integrated_loudness(acc[i * 65536 : i*65536+65536])
+        #    vox_loudness = meter.integrated_loudness(vox[i * 65536 : i*65536+65536])
+        #
+        #    if not np.isfinite(acc_loudness) or not np.isfinite(vox_loudness):
+        #        vox_acc_ratio = np.asarray(np.nan)
+        #    else:
+        #        vox_acc_ratio = vox_loudness - acc_loudness
+        #
+        #    vox_acc_ratio_list.append(vox_acc_ratio)
+
+
+        #extract the overall integrated loudness
+        acc_loudness = meter.integrated_loudness(acc)
+        vox_loudness = meter.integrated_loudness(vox)
+        vox_acc_ratio = vox_loudness - acc_loudness
+
+        vox_acc_ratio_list = []
+        for i in range(int(acc.shape[0] / 65536)):
+            vox_acc_ratio_list.append(vox_acc_ratio)
+
+
+
+
+
 
         def reshape(mel_spec):
             frameNum = mel_spec.shape[1]
-            blockNum = int(frameNum / 64) #(63*1024+2048)/44100 = 1.501s
+            blockNum = int(frameNum / 64) #64*1024/44100 = 1.48s
             mel_spec = mel_spec.T[:-(frameNum - blockNum*64)].T
             mel_spec_matrix = torch.reshape(mel_spec, (128, blockNum, 64))
             mel_spec_matrix = mel_spec_matrix.permute(1, 2, 0) #blocks, time frame, freq frame
@@ -58,37 +104,32 @@ def mel_spec(audio_path, output_path, ground_truth_dict):
         acc_mel_spec = reshape(acc_mel_spec)
         vox_mel_spec = reshape(vox_mel_spec)
 
-        gt_tensor = torch.full((acc_mel_spec.shape[0],), ground_truth)
+
+        if len(vox_acc_ratio_list) != acc_mel_spec.shape[0]:
+            print("ground truth and input data size not match")
+            print(len(vox_acc_ratio_list))
+            print(acc_mel_spec.shape[0])
+
+
+        gt_tensor = torch.tensor(np.asarray(vox_acc_ratio_list)).float()
+
 
         dataset = torch.utils.data.TensorDataset(acc_mel_spec, vox_mel_spec, gt_tensor)
 
         torch.save(dataset, output_path+filename+'.pt')
 
 
+#output_path = "/home/kli421/dir1/musdb18hq_mel/train_snippet/"
+#output_path = "/home/kli421/dir1/musdb18hq_mel/test_snippet/"
 
+#audio_path = "/home/kli421/dir1/musdb18hq/train"
+#audio_path = "/home/kli421/dir1/musdb18hq/test"
 
-#ground_truth_path = "../../data/vox_acc_ratio/train_vox_acc_ratio.json"
-ground_truth_path = "../../data/vox_acc_ratio/test_vox_acc_ratio.json"
-f = open(ground_truth_path)
-ground_truth_dict = json.load(f)
-
-
-#output_path = "/Volumes/mix/Dataset/musdb18hq_mel/train/"
-output_path = "/Volumes/mix/Dataset/musdb18hq_mel/test/"
-
-#audio_path = "/Volumes/mix/Dataset/musdb18hq/train"
-audio_path = "/Volumes/mix/Dataset/musdb18hq/test"
-
-mel_spec(audio_path, output_path, ground_truth_dict)
+#mel_spec(audio_path, output_path)
 
 
 
-"""
-batch_size = 16
-train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+output_path = "/home/kli421/dir1/GTZAN_mel/"
+audio_path = "/home/kli421/dir1/GTZAN/separated/mdx_extra/"
 
-for X_1, X_2, y in train_dataloader:
-    print(X_1.shape)
-    print(X_2.shape)
-    print(y.shape)
-"""
+mel_spec(audio_path, output_path)
---
+++ src/data_collection/old_mel_spec.py
@@ -0,0 +1,94 @@
+import os
+from os import walk
+import json
+import torch
+import torchaudio
+
+def mel_spec(audio_path, output_path, ground_truth_dict):
+    """
+    create mel_spec tensor and level ratio ground truth
+    """
+
+    abs_audio_path = os.path.abspath(audio_path)
+
+    #win_length = n_fft
+    #hop_length = win_length // 2
+    transform = torchaudio.transforms.MelSpectrogram(sample_rate=44100, n_fft=2048)
+
+
+    for (dirpath, dirnames, filenames) in walk(abs_audio_path):
+
+        if "mixture.wav" in filenames \
+            and "vocals.wav" in filenames:
+
+            mxiture_path = dirpath+"/mixture.wav"
+            vox_path = dirpath+"/vocals.wav"
+
+            path_str = str(dirpath)
+            index = path_str.rfind('/')
+            filename = path_str[index+1:]
+            print(filename)
+
+            ground_truth = ground_truth_dict[filename+"--vox_acc_ratio"]
+
+
+        else:
+            continue
+
+        mxiture, sample_rate = torchaudio.load(mxiture_path)
+        vox, sample_rate = torchaudio.load(vox_path)
+
+        acc = mxiture - vox
+
+        acc = torch.mean(acc, 0)
+        vox = torch.mean(vox, 0)
+
+        acc_mel_spec = transform(acc)
+        vox_mel_spec = transform(vox)
+
+
+        def reshape(mel_spec):
+            frameNum = mel_spec.shape[1]
+            blockNum = int(frameNum / 64) #(63*1024+2048)/44100 = 1.501s
+            mel_spec = mel_spec.T[:-(frameNum - blockNum*64)].T
+            mel_spec_matrix = torch.reshape(mel_spec, (128, blockNum, 64))
+            mel_spec_matrix = mel_spec_matrix.permute(1, 2, 0) #blocks, time frame, freq frame
+            return mel_spec_matrix
+
+        acc_mel_spec = reshape(acc_mel_spec)
+        vox_mel_spec = reshape(vox_mel_spec)
+
+        gt_tensor = torch.full((acc_mel_spec.shape[0],), ground_truth)
+
+        dataset = torch.utils.data.TensorDataset(acc_mel_spec, vox_mel_spec, gt_tensor)
+
+        torch.save(dataset, output_path+filename+'.pt')
+
+
+
+
+#ground_truth_path = "../../data/vox_acc_ratio/train_vox_acc_ratio.json"
+ground_truth_path = "../../data/vox_acc_ratio/test_vox_acc_ratio.json"
+f = open(ground_truth_path)
+ground_truth_dict = json.load(f)
+
+
+#output_path = "/Volumes/mix/Dataset/musdb18hq_mel/train/"
+output_path = "/Volumes/mix/Dataset/musdb18hq_mel/test/"
+
+#audio_path = "/Volumes/mix/Dataset/musdb18hq/train"
+audio_path = "/Volumes/mix/Dataset/musdb18hq/test"
+
+mel_spec(audio_path, output_path, ground_truth_dict)
+
+
+
+"""
+batch_size = 16
+train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+
+for X_1, X_2, y in train_dataloader:
+    print(X_1.shape)
+    print(X_2.shape)
+    print(y.shape)
+"""
