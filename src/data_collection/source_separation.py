670a38c103386bd226edd8f3bd0c4c566d179289
“likelian” <likelian@me.com>
2022-09-21 22:01:24 -0400

    separating partial MSD

--- src/data_collection/source_separation.py
+++ src/data_collection/source_separation.py
@@ -4,7 +4,12 @@ import subprocess
 import sys
 
 
-
+def list_files(dir):
+    r = []
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            r.append(os.path.join(root, name))
+    return r
 
 
 
@@ -24,15 +29,41 @@ def separate(input_path, output_path):
 
 
     for file in os.listdir(input_path):
-
         if ".wav" in file:
             each_cmd = cmd + ["-o", str(output_path)]
             each_cmd += [str(input_path+"/"+file)]
             subprocess.run(each_cmd)
 
+
+def list_files(dir):
+    r = []
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            r.append(os.path.join(root, name))
+    return r
     
 
+def separate_from_flie_list(file_list, output_path):
+    cmd = ["python3", "-m", "demucs.separate", "-n", model]
+    if mp3:
+        cmd += ["--mp3", f"--mp3-bitrate={mp3_rate}"]
+    if float32:
+        cmd += ["--float32"]
+    if int24:
+        cmd += ["--int24"]
+    if two_stems is not None:
+        cmd += [f"--two-stems={two_stems}"]
+    
+    if isGPU:
+        cmd += ["-d", "cuda"]
 
+
+    for file in file_list:
+        if ".wav" or ".mp3" in file:
+            print(file)
+            each_cmd = cmd + ["-o", str(output_path)]
+            each_cmd += [file]
+            subprocess.run(each_cmd)
 
     
 # Customize the following options!
@@ -50,10 +81,14 @@ int24 = False    # output as int24 wavs, unused if 'mp3' is True.
 # You cannot set both `float32 = True` and `int24 = True` !!
 
 
-input_path = '/home/kli421/dir1/GTZAN/original'
-output_path = '/home/kli421/dir1/GTZAN/separated'
+#input_path = '/home/kli421/dir1/GTZAN/original'
+#output_path = '/home/kli421/dir1/GTZAN/separated'
 
 
+input_path = "/home/kli421/dir1/MSD/songs"
+output_path = "/home/kli421/dir1/MSD/separated"
 
+all_files = list_files(input_path)
+#print(all_files)
 
-separate(input_path, output_path)
+separate_from_flie_list(all_files, output_path)
