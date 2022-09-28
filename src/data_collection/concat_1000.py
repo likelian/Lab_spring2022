238331180ee1990ffe5425f4a767a033b8ab48fd
“likelian” <likelian@me.com>
2022-09-28 00:59:29 -0400

    load MSD in training

--- src/data_collection/concat_1000.py
+++ src/data_collection/concat_1000.py
@@ -20,7 +20,8 @@ def concat(dataset_path, otuput_path):
             gc.collect()
             counter += 1
         
-
+        #the last group is not dumped......
+        #should be fixed, but not necessary
         if counter >= 1000:
             dataset = torch.utils.data.ConcatDataset(datasets)
             torch.save(dataset, otuput_path+str(dataset_counter)+".pt")
