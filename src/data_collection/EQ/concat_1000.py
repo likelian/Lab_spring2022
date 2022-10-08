f5a26ba9c3b8e6200adfde2da0c60c64a195679b
“likelian” <likelian@me.com>
2022-10-08 00:11:06 -0400

    EQ mel concat

---
+++ src/data_collection/EQ/concat_1000.py
@@ -0,0 +1,63 @@
+import torch
+import os
+import gc
+
+
+def concat(dataset_path, otuput_path):
+
+    datasets = []
+
+    dataset_counter = 0
+    counter = 1
+
+    for file in os.listdir(dataset_path):
+
+        if ".pt" in file:
+            try:
+                data = torch.load(dataset_path+"/"+file)
+            except:
+                print(dataset_path+"/"+file)
+                continue
+
+            print("pass")
+            print("")
+            
+            datasets.append(data)
+            del data
+            gc.collect()
+            counter += 1
+        
+        #the last group is not dumped......
+        #should be fixed, but not necessary
+        if counter >= 1000:
+            dataset = torch.utils.data.ConcatDataset(datasets)
+            torch.save(dataset, otuput_path+str(dataset_counter)+".pt")
+            del datasets[:]
+            del datasets
+            datasets = []
+            del dataset
+            counter = 1
+
+            dataset_counter += 1
+            
+    
+    dataset = torch.utils.data.ConcatDataset(datasets)
+            torch.save(dataset, otuput_path+str(dataset_counter)+".pt")
+            del datasets[:]
+            del datasets
+            datasets = []
+            del dataset
+
+
+
+dataset_path = "/home/kli421/dir1/EQ_mel/musdb18hq/test"
+otuput_path = "/home/kli421/dir1/EQ_mel/musdb18hq/concat/test/"
+
+dataset_path = "/home/kli421/dir1/EQ_mel/musdb18hq"
+concat(dataset_path, otuput_path)
+
+
+dataset_path = "/home/kli421/dir1/EQ_mel/musdb18hq/train"
+otuput_path = "/home/kli421/dir1/EQ_mel/musdb18hq/concat/train/"
+
+concat(dataset_path, otuput_path)
