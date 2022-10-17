50b5eef1d5b36aae282ccff925e0b9863b073c9b
“likelian” <likelian@me.com>
2022-10-17 14:49:09 -0400

    lr=0.001 on 10 pt

--- src/train/EQ_net.py
+++ src/train/EQ_net.py
@@ -160,7 +160,7 @@ def train(model, device, dataset_path, test_path, epochs):
   L1_train_loss = nn.L1Loss()
   L1_validation_loss = nn.L1Loss()
 
-  optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+  optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
   train_loss, validation_loss = [], []
   #batch_train_loss, batch_validation_loss = [], []
@@ -274,7 +274,7 @@ def train(model, device, dataset_path, test_path, epochs):
         #remove!!!!!!!!!!!!!!!
         #only train on the first 1 .pt file, half of all
         counter += 1
-        if counter >= 50:
+        if counter >= 10:
           break
         
 
