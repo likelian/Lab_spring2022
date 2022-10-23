b6a172195f68436a4b6464b40f96e3ffc41cac22
likelian <likelian@me.com>
2022-10-22 23:01:29 -0400

    pack into evolve function

--- src/data_collection/reverb/GA.py
+++ src/data_collection/reverb/GA.py
@@ -8,41 +8,7 @@ from pedalboard import Pedalboard, load_plugin
 import soundfile as sf
 from scipy import signal
 import matplotlib.pyplot as plt
-
-
-
-
-#function_inputs = [4,-2,3.5,5,-11,-4.7]
-#desired_output = 44
-
-
-vst = load_plugin('/Users/likelian/Desktop/Lab/Lab_spring2022/VST3/Mac/FdnReverb.vst3')
-vst.dry_wet = 1.   #0. is 100% dry
-
-#print(1)
-#quit()
-
-input_audio, rate = sf.read('/Users/likelian/Desktop/Lab/Lab_spring2022/data/IRs/test/ M.E.R.C. Music - Knockout.wav')
-input_audio = np.array(input_audio).T
-audio_len = input_audio.shape[1]
-
-
-#delta signal to get the impluse response of the reverb
-delta = np.zeros((2,  6 * rate))
-delta[0][0] = 1.
-delta[1][0] = 1.
-
-
-
-#make the delta signal and the original IR audio the same length
-audio = np.pad(input_audio, ((0, 0), (0, 6 * rate - audio_len)), 'constant')
-
-
-
-f, t, Sxx = signal.spectrogram(audio, rate)
-#plt.pcolormesh(t, f, Sxx[0])
-#plt.show()
-
+import os
 
 
 
@@ -99,7 +65,7 @@ def apply_reverb(solution, vst, delta, rate):
 
     return output
 
-    
+
 
 def fitness_func(solution, solution_idx):
 
@@ -112,64 +78,124 @@ def fitness_func(solution, solution_idx):
 
     print(fitness)
     print(" ")
-    
+
     return fitness
 
 
 
 
+def reverb_search(fitness_func, Sxx_local):
+
+    """
+    Sxx: the spectrogram of the input audio
+    """
+
+    #Sxx needs to be used in fitness_func, but fitness_func is defined by the package
+    #Setting Sxx to be a global variable allowing fitness_func to use Sxx
+    global Sxx 
+    Sxx = Sxx_local
+
+    fitness_function = fitness_func
+
+    num_generations = 1
+    num_parents_mating = 4
+
+    sol_per_pop = 8
+    num_genes = 10
+
+    init_range_low = -2
+    init_range_high = 5
+
+    parent_selection_type = "sss"
+    keep_parents = 1
+
+    crossover_type = "single_point"
+
+    mutation_type = "random"
+    mutation_percent_genes = 10
+
+
+    ga_instance = pygad.GA(num_generations=num_generations,
+                           num_parents_mating=num_parents_mating,
+                           fitness_func=fitness_function,
+                           sol_per_pop=sol_per_pop,
+                           num_genes=num_genes,
+                           init_range_low=init_range_low,
+                           init_range_high=init_range_high,
+                           parent_selection_type=parent_selection_type,
+                           keep_parents=keep_parents,
+                           crossover_type=crossover_type,
+                           mutation_type=mutation_type,
+                           mutation_percent_genes=mutation_percent_genes)
+
+    ga_instance.run()
+
+
+    solution, solution_fitness, solution_idx = ga_instance.best_solution()
+    print("Parameters of the best solution : {solution}".format(solution=solution))
+    print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+
+    return solution
 
 
 
 
 
-fitness_function = fitness_func
-
-num_generations = 1
-num_parents_mating = 4
-
-sol_per_pop = 8
-num_genes = 10
-
-init_range_low = -2
-init_range_high = 5
-
-parent_selection_type = "sss"
-keep_parents = 1
-
-crossover_type = "single_point"
-
-mutation_type = "random"
-mutation_percent_genes = 10
+def evolve(dataset_path, filename):
+    input_audio_path = dataset_path + filename
+    input_audio, rate = sf.read(input_audio_path)
+    input_audio = np.array(input_audio).T
+    audio_len = input_audio.shape[1]
 
 
-
-ga_instance = pygad.GA(num_generations=num_generations,
-                       num_parents_mating=num_parents_mating,
-                       fitness_func=fitness_function,
-                       sol_per_pop=sol_per_pop,
-                       num_genes=num_genes,
-                       init_range_low=init_range_low,
-                       init_range_high=init_range_high,
-                       parent_selection_type=parent_selection_type,
-                       keep_parents=keep_parents,
-                       crossover_type=crossover_type,
-                       mutation_type=mutation_type,
-                       mutation_percent_genes=mutation_percent_genes)
+    #make the delta signal and the original IR audio the same length
+    input_audio = np.pad(input_audio, ((0, 0), (0, 6 * rate - audio_len)), 'constant')
 
 
-ga_instance.run()
+    f, t, Sxx = signal.spectrogram(input_audio, rate)
+    plt.pcolormesh(t, f, Sxx[0], shading='gouraud')
+    plt.savefig(plot_path + filename + ".png")
+    plt.close()
+
+
+    solution = reverb_search(fitness_func, Sxx)
+
+
+    output = apply_reverb(solution, vst, delta, rate)
+    f, t, output_Sxx = signal.spectrogram(output, rate)
+
+    """
+    #write out the audio from outuput
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    """
+
+    plt.pcolormesh(t, f, output_Sxx[0], shading='gouraud')
+    plt.savefig(plot_path + filename + "-output" + ".png")
+    plt.close()
 
 
 
+#path to save the plot
+plot_path = '/Users/likelian/Desktop/Lab/Lab_spring2022/results/reverb matching/'
+
+vst = load_plugin('/Users/likelian/Desktop/Lab/Lab_spring2022/VST3/Mac/FdnReverb.vst3')
+vst.dry_wet = 1.   #0. is 100% dry
+
+dataset_path = "/Users/likelian/Desktop/Lab/Lab_spring2022/data/IRs/test/"
 
 
-solution, solution_fitness, solution_idx = ga_instance.best_solution()
-print("Parameters of the best solution : {solution}".format(solution=solution))
-print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+rate = 48000
+#delta signal to get the impluse response of the reverb
+delta = np.zeros((2,  6 * rate))
+delta[0][0] = 1.
+delta[1][0] = 1.
 
 
-print(output_Sxx)
+for file in os.listdir(dataset_path):
+        if ".wav" in file:
+            evolve(dataset_path, file)
 
-#prediction = np.sum(np.array(function_inputs)*solution)
-#print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
+            print(file)
+
+
+
