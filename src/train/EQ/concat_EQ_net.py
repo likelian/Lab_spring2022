a861314eb87156948aa2ba06a03f90180c645ea5
“likelian” <likelian@me.com>
2022-11-04 22:45:33 -0400

    drop L2

--- src/train/EQ/concat_EQ_net.py
+++ src/train/EQ/concat_EQ_net.py
@@ -171,7 +171,7 @@ def train(model, device, dataset_path, test_path, epochs):
   L1_train_loss = nn.L1Loss()
   L1_validation_loss = nn.L1Loss()
 
-  optimizer = torch.optim.Adam(model.parameters(), lr=1e-6, weight_decay=1e-7)
+  optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=0)
 
   train_loss, validation_loss = [], []
   #batch_train_loss, batch_validation_loss = [], []
