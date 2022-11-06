719b30367fa96c0c109dabdbd9faf38293dbaa97
likelian <likelian@me.com>
2022-11-05 23:59:26 -0400

    tr: read audio

--- src/data_collection/EQ/EQ_concat_mel_spec.py
+++ src/data_collection/EQ/EQ_concat_mel_spec.py
@@ -154,9 +154,16 @@ def EQ_mel_spec(abs_audio_path, output_path):
         else:
             continue
 
-        mxiture, sample_rate = torchaudio.load(mixture_path)
+        try:
+            mxiture, sample_rate = torchaudio.load(mixture_path)
+        except:
+            continue
         #acc, sample_rate = torchaudio.load(acc_path)
-        vox, sample_rate = torchaudio.load(vox_path)
+
+        try:
+            vox, sample_rate = torchaudio.load(vox_path)
+        except:
+            continue
 
         acc = mxiture - vox
 
