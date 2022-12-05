613ac9d8c4ab26e80e1b714a3f82167d3add00aa
likelian <likelian@me.com>
2022-12-04 21:51:01 -0500

    overall csv

--- survey/analysis/src/analysis.py
+++ survey/analysis/src/analysis.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 
-csv_path = "/Users/likelian/Desktop/Lab/Lab_spring2022/survey/results/Automatic Mixing Official_November 26, 2022_10.22.csv"
+csv_path = "/Users/likelian/Desktop/Lab/Lab_spring2022/survey/analysis/overall.csv"
 
 
 df = pd.read_csv(csv_path, encoding = 'unicode_escape', engine ='python')
